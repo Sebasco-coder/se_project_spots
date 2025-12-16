2cc7da740835e08c257b95a5653f1d47d1f900b9
Sebastian Moreno <sebastianrijo04@gmail.com>
2025-12-16 16:34:58 -0500

    smooth transition

--- scripts/index.js
+++ scripts/index.js
@@ -6,57 +6,85 @@ const initialCards = [
 
 {
   name: "Restaurant terrace", 
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg" 
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg" 
 }, 
 
 {
   name: "An outdoor cafe", 
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
 }, 
 
 {
   name: "A very long bridge, over the forest and through the trees", 
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
 }, 
 
 {
   name: "Tunnel with morning light", 
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
 }, 
 
 {
   name: "Mountain house", 
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
 }
 
 ]
 
-const editProfilebtn = document.querySelector(".profile__edit-btn")
-const editProfileModal = document.querySelector("#edit-profile-modal"); 
-const editProfileClosebtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
-const newPostBtn = document.querySelector(".profile__add-btn")
+const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostImageInput = newPostModal.querySelector("#card-image-input");
+const newPostCaptionInput = newPostModal.querySelector("#new-post-caption-input");
 
-editProfilebtn.addEventListener("click", function () {
-    editProfileModal.classList.add("modal_is-opened");
-}); 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
-editProfileClosebtn.addEventListener("click", function () {
-    editProfileModal.classList.remove("modal_is-opened"); 
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
+editProfileBtn.addEventListener("click", () => {
+  openModal(editProfileModal);
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
 });
 
-newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+editProfileForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  closeModal(editProfileModal);
 });
 
-newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+editProfileCloseBtn.addEventListener("click", () => {
+  closeModal(editProfileModal);
 });
 
-initialCards.forEach(function(card) {
-console.log(card.name); 
-console.log(card.link); 
+newPostBtn.addEventListener("click", () => {
+  openModal(newPostModal);
+  newPostImageInput.value = "";
+  newPostCaptionInput.value = "";
+});
 
+newPostForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("New Post submitted:", newPostImageInput.value, newPostCaptionInput.value);
+  closeModal(newPostModal);
+});
+
+newPostCloseBtn.addEventListener("click", () => {
+  closeModal(newPostModal);
 });
